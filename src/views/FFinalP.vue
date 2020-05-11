6361ad5a43ac8f417e40b61a64e2afa9b709fe0a
marcos <marcosvalref@gmail.com>
2020-05-11 17:47:27 +0200

    version 0.2.1 ultimo formulario a medias

--- src/views/FFinalP.vue
+++ src/views/FFinalP.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="FFinalP">
-     <router-link to="/Formulario3">Anexo I</router-link> |
+     <router-link to="/Formulario3">Anexo I</router-link> <br>
      <a href="">Anexo II</a> <br>
-    <a href="">Anexo III</a>
+    <a href="">Anexo III</a> 
   
   </div>
 </template>
